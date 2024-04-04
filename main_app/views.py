6c0007659543a1397b1aa133b22767915ca6aead
Sofiia Kosachevych <sofiakosachvych@gmail.com>
2024-04-03 19:08:37 -0500

    user, model

--- main_app/views.py
+++ main_app/views.py
@@ -1,22 +1,28 @@
 from django.shortcuts import render
 
-# views.py
+from .models import Finch
 
-# Add this cats list below the imports
-finches = [
-  {'name': 'Lolo', 'breed': 'tabby', 'description': 'furry little demon', 'age': 3},
-  {'name': 'Sachi', 'breed': 'calico', 'description': 'gentle and loving', 'age': 2},
-]
-
-# Create your views here.
 def home(request):
-
 	return render(request, 'home.html')
 
 def about(request):
 	return render(request, 'about.html')
 
 def finches_index(request):
-  return render(request, 'finches/index.html', {
-    'finches': finches
+	# tell the model to find all the rows in the cats table!
+	finches = Finch.objects.all()
+	return render(request, 'finches/index.html', {
+		'finches': finches
+		#'cats' becomes a variable name in 'cats/index.html'
+		# just like express 
+		# res.render('cats/index', {'cats': cats})
   })
+
+def finches_detail(request, finch_id):
+	# tell the model to find the row that matches cat_id from the request in the database
+	finch = Finch.objects.get(id=finch_id)
+	return render(request, 'finches/detail.html', {
+		'finch': finch
+		# finch (the key) is the variable name in cats/detail.html 
+	})
+
